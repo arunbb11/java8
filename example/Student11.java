2800471de7b9f99a0d1fe4b07f15a7f14187f9a5
arunbb11 <169126535+arunbb11@users.noreply.github.com>
2024-06-22 12:04:02 +0530

    first commit

---
+++ example/Student11.java
@@ -0,0 +1,17 @@
+/*
+package com.javatechie.lambda.demo.example;
+
+public class Student11 {
+    public int id;
+    public String name;
+    public static void main(String[] args) {
+        Student11 s1=new Student11();
+        s1.id=1;
+        s1.name="Lazy Programmer";
+        Student11 s2=new Student11();
+        s2.id=1;
+        s2.name="Lazy Programmer";
+        System.out.println((s1==s2)+":"+(s1.equals(s2)));
+    }
+}
+*/
